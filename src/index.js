023a7c4f2fea3f5ccee466e6a8e664de0972afe4
Jefry Guevara <jefry_guevara@epam.com>
2022-05-20 16:32:03 -0500

    Installed all dependencies and ready to start

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: ['Public Sans:100,300,400,600', 'sans-serif'],
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +17,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
